359d448015ed801eb65900e4388d68917afa01cd
Steve <ycsteven.tsao@gmail.com>
2020-04-12 15:07:44 +0800

    使用 actions 取得遠端資料

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
@@ -7,15 +8,37 @@ export default new Vuex.Store({
   strict: true,
   state: {
     isLoading: false,
+    products: [],
+    categories: [],
   },
   actions: {
     updateLoading(context, status) {
       context.commit('LOADING', status);
     },
+    getProducts(context) {
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`;
+      context.commit('LOADING', true);
+      axios.get(url).then((response) => {
+        context.commit('PRODUCTS', response.data.products);
+        context.commit('CATEGORIES', response.data.products);
+        console.log('取得產品列表:', response);
+        context.commit('LOADING', false);
+      });
+    },
   },
   mutations: {
     LOADING(state, status) {
       state.isLoading = status;
+    },
+    PRODUCTS(state, payload) {
+      state.products = payload;
+    },
+    CATEGORIES(state, payload) {
+      const categories = new Set();
+      payload.forEach((item) => {
+        categories.add(item.category);
+      });
+      state.categories = Array.from(categories);
     },
   },
 });
